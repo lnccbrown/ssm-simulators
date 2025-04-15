92c17b354f9f9725d836d8ae571560eeba4dde8e
Carlos Paniagua <cpaniaguam@gmail.com>
2025-04-15 15:04:00 -0400

    Refactor DDM model configuration to use boundary functions directly

--- ssms/config/_modelconfig/ddm.py
+++ ssms/config/_modelconfig/ddm.py
@@ -1,7 +1,7 @@
 """DDM model configuration."""
 
 import cssm
-from .base import boundary_config
+from ssms.basic_simulators import boundary_functions as bf
 
 
 def get_ddm_config():
@@ -11,7 +11,7 @@ def get_ddm_config():
         "params": ["v", "a", "z", "t"],
         "param_bounds": [[-3.0, 0.3, 0.1, 0.0], [3.0, 2.5, 0.9, 2.0]],
         "boundary_name": "constant",
-        "boundary": boundary_config["constant"]["fun"],
+        "boundary": bf.constant,
         "boundary_params": [],
         "n_params": 4,
         "default_params": [0.0, 1.0, 0.5, 1e-3],
