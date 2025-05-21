35994c210ca5a4e76d2751d3f12691a4a2b665b6
Carlos Paniagua <cpaniaguam@gmail.com>
2025-05-21 14:41:35 -0400

    Update init in dataset generator

--- ssms/__init__.py
+++ ssms/__init__.py
@@ -1,25 +1,16 @@
-"""SSM Simulators package."""
+# ssms/__init__.py
+from . import basic_simulators
+from . import dataset_generators
+from . import _config
+from . import support_utils
+from . import boundary_functions
+from . import drift_functions
 
-__version__ = "0.9.0"  # importlib.metadata.version(__package__ or __name__)
-
-# Import core functionality first
-from . import boundary_functions  # noqa: F401
-from . import drift_functions  # noqa: F401
-
-# Then import config which depends on core functions
-from . import config  # noqa: F401
-
-# Then import simulators which depend on config
-from . import basic_simulators  # noqa: F401
-
-# Finally import higher level modules
-from . import dataset_generators  # noqa: F401
-from . import support_utils  # noqa: F401
-
+__version__ = "0.9.0b1"
 __all__ = [
     "basic_simulators",
     "dataset_generators",
-    "config",
+    "_config",
     "support_utils",
     "boundary_functions",
     "drift_functions",
