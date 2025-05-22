07b9d99c55d47dcb8b43a823f67eb6cec2ac146f
Carlos Paniagua <cpaniaguam@gmail.com>
2025-05-22 14:25:59 -0400

    Refactor data generator configuration and add convenience functions for default settings

--- ssms/config/__init__.py
+++ ssms/config/__init__.py
@@ -1,11 +1,21 @@
 from .config import (  # noqa: D104
     boundary_config,
     boundary_config_to_function_params,
-    data_generator_config,
     drift_config,
     kde_simulation_filters,
     model_config,
+    DeprecatedDict,
+    get_kde_simulation_filters,
+    get_opn_only_config,
+    get_cpn_only_config,
+    get_lan_config,
+    get_ratio_estimator_config,
+    get_defective_detector_config,
+    get_snpe_config,
+    get_default_generator_config,
+    data_generator_config,
 )
+
 from .kde_constants import KDE_NO_DISPLACE_T  # noqa: F401
 
 __all__ = [
@@ -15,4 +25,13 @@ __all__ = [
     "boundary_config",
     "drift_config",
     "boundary_config_to_function_params",
+    "DeprecatedDict",
+    "get_kde_simulation_filters",
+    "get_opn_only_config",
+    "get_cpn_only_config",
+    "get_lan_config",
+    "get_ratio_estimator_config",
+    "get_defective_detector_config",
+    "get_snpe_config",
+    "get_default_generator_config",
 ]
