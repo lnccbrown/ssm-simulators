ce9ad19b7e58a0c9fda35fd1abb5bec1a2e2ff47
Carlos Paniagua <cpaniaguam@gmail.com>
2025-06-27 10:47:45 -0400

    Update version to 0.9.0 and refactor setup.py for package discovery

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
-# from distutils.core import setup
-# from xml.etree.ElementInclude import include
-from setuptools import setup, Extension
+from setuptools import setup, Extension, find_packages
 import numpy
 
+# Try to build with Cython if available
 try:
     from Cython.Build import cythonize
 
@@ -13,14 +12,26 @@ try:
 except ImportError:
     ext_modules = [Extension("cssm", ["src/cssm.pyx"], language="c++")]
 
+# Use find_packages to automatically discover all packages
+packages = find_packages(include=["ssms", "ssms.*"])
+
 setup(
-    packages=[
-        "ssms",
-        "ssms.basic_simulators",
-        "ssms.config",
-        "ssms.dataset_generators",
-        "ssms.support_utils",
-    ],
+    name="ssm-simulators",
+    version="0.9.0",
+    packages=packages,
+    package_data={
+        "ssms": ["**/*.py", "**/*.pyx", "**/*.pxd", "**/*.so", "**/*.pyd"],
+    },
+    include_package_data=True,
     include_dirs=[numpy.get_include()],
     ext_modules=ext_modules,
+    install_requires=[
+        "numpy",
+        "pandas",
+        "scipy",
+        "matplotlib",
+        "tqdm",
+        "pyyaml",
+        "typer",
+    ],
 )
