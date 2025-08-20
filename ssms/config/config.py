237ae5368e24e5a18844d090449a7ea429090229
AndrewZhang599 <andrewzhang5681@gmail.com>
2025-08-20 15:38:45 -0400

    update bounds p_magnitude and r to 8 and 4 respectively

--- ssms/config/config.py
+++ ssms/config/config.py
@@ -447,8 +447,8 @@ model_config = {
             "sda",
         ],
         "param_bounds": [
-            [0.3, 0.1, 1e-3, 2.0, -7, 0.01, 1e-1],
-            [3.0, 0.9, 2.0, 7, 7, 3, 3],
+            [0.3, 0.1, 1e-3, 2.0, -8, 0.01, 1e-1],
+            [3.0, 0.9, 2.0, 8, 8, 4, 3],
         ],
         "boundary_name": "constant",
         "boundary": bf.constant,
