6a5381ca5fe61a568ab83137bda4c2d7427dae0c
Alexander Fengler <alexanderfengler@gmx.de>
2026-08-24 01:20:17 -0400

    feat: classify built-in observation producers

--- ssms/config/_modelconfig/inv_temp_softmax.py
+++ ssms/config/_modelconfig/inv_temp_softmax.py
@@ -21,6 +21,14 @@ def _get_inv_temp_softmax_config(n_choices: int) -> dict:
         "default_params": [1.0, *([0.5] * n_choices)],
         "nchoices": n_choices,
         "choices": list(range(n_choices)),
+        "observation_schema_version": 1,
+        "observation_schema": (
+            {
+                "name": "response",
+                "kind": "categorical",
+                "values": tuple(range(n_choices)),
+            },
+        ),
         "n_particles": 1,
         "tags": ["choice_only_rl"],
         "simulator": inv_temp_softmax,
