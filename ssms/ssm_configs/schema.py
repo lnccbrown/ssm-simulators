f9ed11392680ec3b41fd824067d68a9b8de65786
Paul Xu <yang_xu@brown.edu>
2026-09-04 11:15:19 -0400

    feat: added simple base Pydantic classes

---
+++ ssms/ssm_configs/schema.py
@@ -0,0 +1,19 @@
+from dataclasses import field
+
+from pydantic import BaseModel
+
+# ====== Centralized SSM defaults =====
+DEFAULT_SSM_OBSERVED_DATA = ["rt", "response"]
+DEFAULT_SSM_CHOICES = (0, 1)
+
+
+# An example of a base config schema for SSMs. This can be extended for specific SSMs such as RLSSMs.
+class BaseConfigSchema(BaseModel):
+    name: str
+    description: str | None = None
+
+    response: list[str] = DEFAULT_SSM_OBSERVED_DATA
+    choices: tuple[int, ...] | None = DEFAULT_SSM_CHOICES
+
+    list_params: dict[str, list] | None = None
+    bounds: dict[str, tuple[float, float]] = field(default_factory=dict)
